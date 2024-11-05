e64debe61d9703d408b95ca3af2a623c130d1506
avataraad@gmail.com <raad@Raads-MBP.attlocal.net>
2024-11-05 16:26:39 -0500

    Add basic home screen and navigation

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+const Stack = createNativeStackNavigator();
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
+
+export default App;
